20ac0e60256014ffd5e21d04e761ef1f1bb4518c
Measrainsey Meng <measrainsey@pm.me>
2025-04-22 16:47:35 +0200

    Add constraints to pypsa network

--- plexos_pypsa/scripts/create_model.py
+++ plexos_pypsa/scripts/create_model.py
@@ -1,23 +1,28 @@
 import pypsa  # type: ignore
 from plexosdb import PlexosDB  # type: ignore
 
-from plexos_pypsa.network.add import add_buses, add_generators, add_lines, add_storage
+from plexos_pypsa.network.add import (
+    add_buses,
+    add_constraints,
+    add_generators,
+    add_lines,
+    add_storage,
+)
 
-# Load PLEXOS input XML
+# load PLEXOS input XML
 file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
 plexos_db = PlexosDB.from_xml(file_xml)
 
-# Initialize PyPSA network
+# initialize PyPSA network
 network = pypsa.Network()
 
-# Add buses, generators, and lines to the network
+# add to PyPSA network
 add_buses(network, plexos_db)
 add_generators(network, plexos_db)
 add_lines(network, plexos_db)
 add_storage(network, plexos_db)
+add_constraints(network, plexos_db)
 
-
-# list generators in network
-# Optional: Save to file
+# save to file
 network.export_to_netcdf("converted_network.nc")
 print("Network exported to converted_network.nc")
