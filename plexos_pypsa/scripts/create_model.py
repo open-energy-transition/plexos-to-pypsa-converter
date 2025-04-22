83e14b0dca25354113449452f633f64a7e926866
Measrainsey Meng <measrainsey@pm.me>
2025-04-22 15:52:41 +0200

    Add storage to pypsa networks

--- plexos_pypsa/scripts/create_model.py
+++ plexos_pypsa/scripts/create_model.py
@@ -1,7 +1,7 @@
 import pypsa  # type: ignore
 from plexosdb import PlexosDB  # type: ignore
 
-from plexos_pypsa.network.add import add_buses, add_generators, add_lines
+from plexos_pypsa.network.add import add_buses, add_generators, add_lines, add_storage
 
 # Load PLEXOS input XML
 file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
@@ -10,10 +10,12 @@ plexos_db = PlexosDB.from_xml(file_xml)
 # Initialize PyPSA network
 network = pypsa.Network()
 
-# --- Execute all steps ---
+# Add buses, generators, and lines to the network
 add_buses(network, plexos_db)
 add_generators(network, plexos_db)
 add_lines(network, plexos_db)
+add_storage(network, plexos_db)
+
 
 # list generators in network
 # Optional: Save to file
