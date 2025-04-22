894fa62a8b73892c8ae9f93971db346cdc451625
Measrainsey Meng <measrainsey@pm.me>
2025-04-22 16:55:58 +0200

    Add check for constraints

--- plexos_pypsa/scripts/create_model.py
+++ plexos_pypsa/scripts/create_model.py
@@ -8,6 +8,7 @@ from plexos_pypsa.network.add import (
     add_lines,
     add_storage,
 )
+from plexos_pypsa.network.summarize import check_constraints
 
 # load PLEXOS input XML
 file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
@@ -19,9 +20,11 @@ network = pypsa.Network()
 # add to PyPSA network
 add_buses(network, plexos_db)
 add_generators(network, plexos_db)
-add_lines(network, plexos_db)
 add_storage(network, plexos_db)
+add_lines(network, plexos_db)
 add_constraints(network, plexos_db)
+
+check_constraints(network)
 
 # save to file
 network.export_to_netcdf("converted_network.nc")
