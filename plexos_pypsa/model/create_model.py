9b7fbb8813885e2cc7f3906da2bd02621a9dbd60
Measrainsey Meng <measrainsey@pm.me>
2025-05-13 05:14:25 -0700

    Add snapshot and load to network

--- plexos_pypsa/model/create_model.py
+++ plexos_pypsa/model/create_model.py
@@ -5,7 +5,9 @@ from plexos_pypsa.network.add import (
     add_buses,
     add_constraints,
     add_generators,
-    add_lines,
+    add_links,
+    add_loads,
+    add_snapshot,
     add_storage,
 )
 from plexos_pypsa.network.summarize import check_constraints
@@ -13,6 +15,9 @@ from plexos_pypsa.network.summarize import check_constraints
 # list XML file
 file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
 # file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/sem/2024-2032/SEM PLEXOS Forecast Model 2024-2032( Public Version)/PUBLIC Validation 2024-2032 Model 2025-03-14.xml"
+
+# specify demand data path
+path_demand = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/Traces/demand"
 
 # load PlexosDB from XML file
 plexos_db = PlexosDB.from_xml(file_xml)
@@ -24,9 +29,12 @@ network = pypsa.Network()
 add_buses(network, plexos_db)
 add_generators(network, plexos_db)
 add_storage(network, plexos_db)
-add_lines(network, plexos_db)
-add_constraints(network, plexos_db)
+add_links(network, plexos_db)
+add_snapshot(network, path_demand)
+add_loads(network, path_demand)
 
+# TODO: fix
+add_constraints(network, plexos_db)
 check_constraints(network)
 
 # save to file
