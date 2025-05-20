cb3f437f1ee09a9fc70ea2d976ab20a071fd1901
Measrainsey Meng <measrainsey@pm.me>
2025-05-20 12:26:13 +0200

    Set `p_max_pu` time series of network

--- plexos_pypsa/model/create_model.py
+++ plexos_pypsa/model/create_model.py
@@ -11,6 +11,7 @@ from plexos_pypsa.network.add import (
     add_snapshot,
     add_storage,
     add_vre_profiles,
+    set_capacity_ratings,
 )
 from plexos_pypsa.network.summarize import check_constraints
 
@@ -30,10 +31,11 @@ network = pypsa.Network()
 
 # add to PyPSA network
 add_buses(network, plexos_db)
+add_snapshot(network, path_demand)
 add_generators(network, plexos_db)
+set_capacity_ratings(network, plexos_db)
 add_storage(network, plexos_db)
 add_links(network, plexos_db)
-add_snapshot(network, path_demand)
 add_loads(network, path_demand)
 add_vre_profiles(network, plexos_db, path_aemo)
 add_hydro_inflows(network, plexos_db, path_aemo)
@@ -45,3 +47,5 @@ check_constraints(network)
 # save to file
 network.export_to_netcdf("converted_network.nc")
 print("Network exported to converted_network.nc")
+
+network.generators_t.p_max_pu["ADPPV1"]
