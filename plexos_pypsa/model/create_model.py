01e2ab2e24965a825256918fd20c79dc9d0b752c
Measrainsey Meng <measrainsey@pm.me>
2025-05-26 06:48:17 +0200

    Rename path

--- plexos_pypsa/model/create_model.py
+++ plexos_pypsa/model/create_model.py
@@ -10,7 +10,7 @@ from plexos_pypsa.network.generators import (
     set_generator_efficiencies,
     set_vre_profiles,
 )
-from plexos_pypsa.network.links import add_links
+from plexos_pypsa.network.links import add_links, set_link_flows
 from plexos_pypsa.network.storage import add_hydro_inflows, add_storage
 from plexos_pypsa.network.summarize import check_constraints
 
@@ -18,8 +18,8 @@ from plexos_pypsa.network.summarize import check_constraints
 file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
 # file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/sem/2024-2032/SEM PLEXOS Forecast Model 2024-2032( Public Version)/PUBLIC Validation 2024-2032 Model 2025-03-14.xml"
 
-# specify demand data path
-path_aemo = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change"
+# specify renewables profiles and demand paths
+path_ren = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change"
 path_demand = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/Traces/demand"
 
 # load PlexosDB from XML file
@@ -39,10 +39,11 @@ add_generators(network, plexos_db)
 set_generator_efficiencies(network, plexos_db, use_incr=True)
 set_capital_costs(network, plexos_db)
 set_capacity_ratings(network, plexos_db)
-set_vre_profiles(network, plexos_db, path_aemo)
+set_vre_profiles(network, plexos_db, path_ren)
 
 # add links
 add_links(network, plexos_db)
+set_link_flows(network, plexos_db)
 
 # add demand/loads
 add_loads(network, path_demand)
@@ -52,7 +53,7 @@ add_loads(network, path_demand)
 
 # add storage (TODO: fix)
 add_storage(network, plexos_db)
-add_hydro_inflows(network, plexos_db, path_aemo)
+add_hydro_inflows(network, plexos_db, path_ren)
 
 # add constraints (TODO: fix)
 add_constraints(network, plexos_db)
