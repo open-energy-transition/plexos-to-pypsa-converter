7058826ba686736f5bce35be843d00f16aad582c
Measrainsey Meng <measrainsey@pm.me>
2025-07-25 16:55:41 +0200

    Create db for different XML

--- plexos_pypsa/scratch/create_db.py
+++ plexos_pypsa/scratch/create_db.py
@@ -1,6 +1,37 @@
-from coad.COAD import COAD
+from coad.COAD import COAD # type: ignore
+from plexos_pypsa.db.models import INPUT_XMLS
+from plexosdb.enums import ClassEnum  # type: ignore
+from plexos_pypsa.db.plexosdb import PlexosDB  # type: ignore
 
-file_xml = "/Users/meas/Library/CloudStorage/GoogleDrive-measrainsey.meng@openenergytransition.org/My Drive/open-tyndp/aemo/2024/2024 ISP Model/2024 ISP Progressive Change/2024 ISP Progressive Change Model.xml"
+file_xml = INPUT_XMLS["plexos-world-2015"]
 
 # this automatically creates a sqlite database from the xml file in the same directory
 coad = COAD(file_xml)
+coad.list("Model")
+coad["Model"]["PLEXOS World 2015 Asia"].dump()
+
+
+# load PlexosDB from XML file
+mod_db = PlexosDB.from_xml(file_xml)
+
+mod_db.list_objects_by_class(ClassEnum.Model)
+mod_db.list_objects_by_class(ClassEnum.STSchedule)
+mod_db.list_objects_by_class(ClassEnum.MTSchedule)
+mod_db.list_objects_by_class(ClassEnum.Horizon)
+mod_db.list_objects_by_class(ClassEnum.Scenario)
+mod_db.list_objects_by_class(ClassEnum.Scenario)
+
+[
+    "Long Term",
+    "Nodal",
+    "PLEXOS World 2015 Africa",
+    "PLEXOS World 2015 Asia",
+    "PLEXOS World 2015 Europe",
+    "PLEXOS World 2015 Global",
+    "PLEXOS World 2015 North America",
+    "PLEXOS World 2015 Oceania",
+    "PLEXOS World 2015 South America",
+    "Regional",
+    "Reliability",
+]
+]
